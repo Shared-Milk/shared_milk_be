72084be5ac4b026e38aac277818d7e0e93882995
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-04-03 17:24:53 -0600

    Add extra expectations for more robust test

--- spec/graphql/queries/users/get_active_users_spec.rb
+++ spec/graphql/queries/users/get_active_users_spec.rb
@@ -18,9 +18,25 @@ module Queries
           mom3.requests.create!(requested_at: 4.week.ago)
 
           result = SharedMilkSchema.execute(query).as_json
-    
+
           expect(result["data"]["activeUsers"].count).to eq(2)
           expect(result["data"]["activeUsers"]).not_to include(mom3)
+          user_data = result["data"]["activeUsers"]
+
+          user_data.each do |donor|
+            expect(donor).to have_key("name")
+            expect(donor["name"]).to be_a(String)
+            expect(donor).to have_key("id")
+            expect(donor["id"]).to be_a(String)
+            expect(donor).to have_key("email")
+            expect(donor["email"]).to be_a(String)
+            expect(donor).to have_key("phoneFormatter")
+            expect(donor["phoneFormatter"]).to be_a(String)
+            expect(donor).to have_key("bio")
+            expect(donor["bio"]).to be_a(String)
+            expect(donor).to have_key("location")
+            expect(donor["location"]).to be_a(String)
+          end 
         end
 
         def query
@@ -31,6 +47,8 @@ module Queries
               id
               email
               phoneFormatter
+              bio
+              location
             }
           }
           GQL
