e9b72281a86ae25d72f6d2bee51668ab2337c842
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-04-05 11:06:55 -0600

    Plant some seeds for the CircleCI gods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# mom_1 = User.create!(name: "Samantha",
-# email: "so@gmail.com",
-# phone: "4231563232",
-# bio: "abc",
-# location: "Denver, CO",
-# donor_status: 0)
-#
-# mom_2 = User.create!(name: "Betty",
-# email: "beegee@gmail.com",
-# phone: "4231563232",
-# bio: "abc",
-# location: "Denver, CO",
-# donor_status: 0,)
-#
-# mom_3 = User.create!(name: "Susana",
-# email: "osusana@gmail.com",
-# phone: "4231563232",
-# bio: "abc",
-# location: "Denver, CO",
-# donor_status: 0)
+mom_1 = User.create!(name: "Samantha",
+email: "so@gmail.com",
+phone: "4231563232",
+bio: "Banana",
+location: "Denver, CO",
+donor_status: 0)
 
-# mom_1.requests.create!(requested_at: 2.week.ago)
-# mom_1.requests.create!(requested_at: 2.days.ago)
-#
-# mom_2.requests.create!(requested_at: 4.week.ago)
-# mom_2.requests.create!(requested_at: 3.week.ago)
-#
-# mom_3.requests.create!(requested_at: 5.week.ago)
-# mom_3.requests.create!(requested_at: 4.week.ago)
+mom_2 = User.create!(name: "Betty",
+email: "beegee@gmail.com",
+phone: "4231563232",
+bio: "Banana milk",
+location: "Denver, CO",
+donor_status: 0,)
+
+mom_3 = User.create!(name: "Susana",
+email: "osusana@gmail.com",
+phone: "4231563232",
+bio: "Banana milkshake",
+location: "Denver, CO",
+donor_status: 0)
+
+mom_1.requests.create!(requested_at: 2.week.ago)
+mom_1.requests.create!(requested_at: 2.days.ago)
+
+mom_2.requests.create!(requested_at: 4.week.ago)
+mom_2.requests.create!(requested_at: 3.week.ago)
+
+mom_3.requests.create!(requested_at: 5.week.ago)
+mom_3.requests.create!(requested_at: 4.week.ago)
