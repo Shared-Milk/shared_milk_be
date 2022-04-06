5543ee7199294903bbf78586c97d633148560560
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-04-06 13:43:04 -0600

    Removed field/method that is erroneous

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,21 +4,11 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
     # /users
     field :users, [Types::UserType], null: false
 
     def users
       User.where(active_status: 0)
-    end
-
-    field :active_users, [Types::UserType], null: false
-
-    def active_users
-      User.only_active
     end
 
     field :user, Types::UserType, null: false do
